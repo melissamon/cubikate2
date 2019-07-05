b0c6fc63554e0a6a316a88e88723684641dc9832
Cristian Echeverría <cecheverria21@gmail.com>
2019-07-05 12:40:26 -0400

    Esta actualización tiene los mantenedores clientes y maestros

---
+++ app/src/main/java/com/example/grupoc/cubikate/frm_alta_maestros.java
@@ -0,0 +1,13 @@
+package com.example.grupoc.cubikate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class frm_alta_maestros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_alta_maestros);
+    }
+}
