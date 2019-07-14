2813c74d728a30dc55102d77d921c91bb985cfae
Cristian Echeverría <cecheverria21@gmail.com>
2019-07-14 10:43:50 -0400

    Prototipo de conexion con bdMaria remoto

--- app/src/main/java/com/example/grupoc/cubikate/MainActivity.java
+++ app/src/main/java/com/example/grupoc/cubikate/MainActivity.java
@@ -1,38 +1,96 @@
 package com.example.grupoc.cubikate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    RequestQueue rq;
+    JsonRequest jrq;
+
+    private EditText etUsuario, etContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsuario = (EditText)findViewById(R.id.txtUsuario);
+        etContrasenia = (EditText)findViewById(R.id.txtContrasenia);
+
+        rq = Volley.newRequestQueue(getApplicationContext());
+
+//        SharedPreferences spInicio = getSharedPreferences("dtCubikate", Context.MODE_PRIVATE);
+//        etUsuario.setText(spInicio.getString("usuario", ""));
+//        etContrasenia.setText(spInicio.getString("contrasenia", ""));
+
+
     }
+
+    public void btnIngresarIngresar_click(View view){
+        iniciarSesion();
+    }
+
 
     //Metodo para el boton clientes btnCliente
-    public void btnclientes_click(View view){
-        Intent btnclientes_click = new Intent(this, frmAltaUsuario.class);
-
-        startActivity(btnclientes_click);
+    public void btnIngresarCliente_click(View view){
+        Intent btnIngresarCliente_click = new Intent(this, frmAltaUsuario.class);
+        startActivity(btnIngresarCliente_click);
     }
 
-    //Metodo para el boton maestros btnMaestro
-    public void btnMaestros_click(View view){
-        Intent btnMaestros_click = new Intent(this, frm_alta_maestros.class);
-
-        startActivity(btnMaestros_click);
+    //Metodo para el boton clientes btnAceptar2
+    public void btnIngresarMaestro_click(View view){
+        Intent btnIngresarMaestro_click = new Intent(this, frm_alta_maestros.class);
+        startActivity(btnIngresarMaestro_click);
     }
 
-    //Metodo para el boton BuscarMAestro btnBuscarMaestro
-    public void btnBuscarMaestro_click(View view){
-        Intent btnBuscarMaestro_click = new Intent(this, frmBuscarMaestro.class);
 
-        startActivity(btnBuscarMaestro_click);
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getApplicationContext(),"No se encontró el usuario "+error.toString(),Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public void onResponse(JSONObject response) {
+        User usuario = new User();
+        Toast.makeText(getApplicationContext(),"Se ha encontrado el usuario "+etUsuario.getText().toString(),Toast.LENGTH_SHORT).show();
+
+        JSONArray jsonArray = response.optJSONArray("datos");
+        JSONObject jsonObject = null;
+
+        try{
+            jsonObject = jsonArray.getJSONObject(0);
+            usuario.setPwd(jsonObject.optString("user"));
+            usuario.setPwd(jsonObject.getString("owd"));
+            usuario.setNames(jsonObject.optString("names"));
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void iniciarSesion() {
+        String url = "http://192.168.1.160/login/sesion.php?user="+etUsuario.getText().toString()+"&pwd="+etContrasenia.getText().toString();
+        jrq = new JsonObjectRequest(Request.Method.GET, url, null,this,this);
+        rq.add(jrq);
+    }
 
 }
