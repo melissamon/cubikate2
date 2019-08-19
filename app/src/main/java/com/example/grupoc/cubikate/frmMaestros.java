0d9223485ca9ed4a0f25fe562427ba3590843de3
Cristian Echeverría <cecheverria21@gmail.com>
2019-08-18 22:53:20 -0400

    Terminada el modulo de Maestro no Disponible

--- app/src/main/java/com/example/grupoc/cubikate/frmMaestros.java
+++ app/src/main/java/com/example/grupoc/cubikate/frmMaestros.java
@@ -60,6 +60,9 @@ public class frmMaestros extends AppCompatActivity{
     public void btnMarcarUbicacionMaestro_click(View view){
         MarcarUbicacionMaestro();
     }
+    public void btnMaestroNoDisponible_click(View view){
+        MaestroNoDisponible();
+    }
     public void btnBuscarCliente_click(View view){
         BuscarCliente();
     }
@@ -109,7 +112,33 @@ public class frmMaestros extends AppCompatActivity{
         requestQueue.add(stringRequest);
     }
 
+    private void MaestroNoDisponible() {
+        String URL;
 
+        URL = "https://uniacc.000webhostapp.com/cubikate/desmarcar_ubicacion_maestro.php";
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "No disponible", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String, String>();
+                parametros.put("RUT",RUT);
+
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 
     private void BuscarCliente() {
         //Intent btnBuscarCliente_click = new Intent(this, frmBuscarCliente.class);
